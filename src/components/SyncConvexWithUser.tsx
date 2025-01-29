8edfa37c3b7dec50b37f454c43dc3aaa7b8a3b5a
amercado <arnold.mercado@hotmail.com>
2025-01-29 15:01:45 +0800

    Clerk user update with convex

---
+++ src/components/SyncConvexWithUser.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import React, { useEffect } from "react";
+import { api } from "../../convex/_generated/api";
+
+
+const SyncConvexWithUser = () => {
+   
+   const {user} = useUser();
+   const updateUser = useMutation(api.users.updateUser);
+
+   useEffect(() => {
+      const syncUser = async () => {
+         try {
+            await updateUser({
+               userId: user?.id ?? "",
+               name: `${user?.firstName} ${user?.lastName}`.trim() ?? "",
+               email: user?.emailAddresses[0].emailAddress ?? "",
+            })
+         }
+         catch(e){
+            console.error(e);
+         }
+      }
+      syncUser();
+   }, [user, updateUser]);
+
+   return <div>SyncConvexWithUser</div>;
+};
+
+export default SyncConvexWithUser;
